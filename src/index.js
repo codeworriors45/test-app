7a86d7530974e9fd8fca8d1b977f1efda1efc40f
naresh parmar <ncparmar8888@gmail.com>
2022-01-30 17:52:02 +0530

    env setup

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:3001/api/"
+axios.defaults.baseURL = process.env.REACT_APP_BASE_URL
 
 ReactDOM.render(
   <React.StrictMode>
